27df1d2034cb54deeb0f9aa60395b1fab98c26af
jasonjoby <jason.joby072@gmail.com>
2021-03-30 20:46:25 +0530

    bignus p5js added

--- sketch.js
+++ sketch.js
@@ -1,9 +1,38 @@
+let gap = 20;
+let angle = 0;
 function setup() {
-    createCanvas(400, 400);
-  }
+  createCanvas(windowWidth, windowHeight);
+  angleMode(degrees);
+  frameRate(24)
+}
+
+function draw() {
+  background(0);
+  noCursor();
+  //fill('white');
+  //circle(mouseX, mouseY, 10);
+  angle++;
+  push();
   
-  function draw() {
-    background(20);
-    ellipse(width/2, height/2, 10);
-  }
+  translate(width/2, height/2.5);
+  noFill();
+  stroke('white');
+  strokeWeight(1);
+  for (let i=0; i<=15; i+=1)
+    {arc(0, 0, 10+i*gap, 10+i*gap, 0, 360 - i - angle/10);}
   
+  pop();
+  
+  stroke('white');
+  fill('white');
+  strokeWeight(1);
+  for (let i=0; i<=50; i+=1)
+    {point(random(width),random(height));}
+  push();
+  translate(width/2, height-height/2.5);
+  textFont('Arial');
+  textSize(50);
+  textAlign(CENTER, CENTER);
+  text('bignus',0,120);
+  pop();
+}
