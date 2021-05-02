82f0436a21014c1d03b744d79b2a243e498144bd
jasonjoby <jason.joby072@gmail.com>
2021-05-03 01:08:35 +0530

    starting with boids

---
+++ sketch_boid.js
@@ -0,0 +1,13 @@
+const flock = [];
+function setup() {
+    createCanvas(640, 360);
+    flock.push(new Boid());
+}
+
+function draw() {
+    background(51);    
+
+    for (let boid of flock) {
+        boid.show();
+    }
+}
