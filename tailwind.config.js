7b319e060d0a1bf7f79940ce1276b9c8f1d2c6ee
jasonjoby <jason.joby072@gmail.com>
2021-10-23 22:20:11 +0530

    Tailwind Init for v2.0

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        primary: '#333333',
+        secondary: '#ff90a5'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
